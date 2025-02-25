9a9ee7c090493fa4cdfc6f5941d20c9d4121a408
2eebyeonghyun <wada0012@naver.com>
2025-02-25 11:02:37 +0900

    250225 update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist", // 기본값이 'dist'라서 따로 설정 안 해도 dist로 생성됨
+  },
   plugins: [react()],
   resolve: {
     alias: {
